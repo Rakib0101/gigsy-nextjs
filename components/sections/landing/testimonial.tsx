c697b647375b1a803731ca872983ac833e4ad689
Rakib0101 <rakib.01jan@gmail.com>
2025-10-31 20:27:21 +0600

    wip

--- components/sections/landing/testimonial.tsx
+++ components/sections/landing/testimonial.tsx
@@ -5,7 +5,7 @@ const Testimonial = () => {
         <div className="">
           <div className="text-center flex flex-col items-center gap-2 md:gap-4 max-w-[1100px] mx-auto">
             <p className="text-[60px] md:text-[80px] lg:text-[100px] leading-[0.3] md:leading-[0.3] lg:leading-[30px] text-white">
-              "
+              &ldquo;
             </p>
             <h2 className="text-white text-lg md:text-2xl lg:text-[34px] font-bold max-w-[549px] px-2">
               You have been amazing , thank you so making this such an easy
